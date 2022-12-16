c14bee6cfe5a7cc18f5bfc3da20d4bbccbb7c080
Mauricio Borawski <mauricio.borawski@gmail.com>
2022-12-16 14:45:31 -0300

    WIP first page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		  },
+		  
+		extend: {
+			colors: {
+			pastel: {
+				green: "#98FF98",
+				secondary: "#E6E6E6"
+			}
+		  },},
 	},
 	plugins: [],
 }
