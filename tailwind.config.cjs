8da18106cb0bbedee1792e32d92fe41c7d369985
Mauricio Borawski <mauricio.borawski@gmail.com>
2023-01-03 19:39:18 -0300

    Style improvements and added the social navbar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -14,7 +14,8 @@ module.exports = {
 			pastel: {
 				greenPrimary: "#7cfc00",
 				greenHover: "rgba(124, 252, 0, .1)",
-				greyPerl: "#E6E6E6"
+				greyPerl: "#E6E6E6",
+				lightDark: "#434343"
 			}
 		  },
 		  width: {
