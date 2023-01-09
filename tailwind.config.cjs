25dd1e5600a4c256ce0803a4d139b2c87d29e511
Mauricio Borawski <mauricio.borawski@gmail.com>
2023-01-09 16:55:14 -0300

    Add experience section and projects

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,27 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		screens: {
-			sm: '480px',
-			md: '768px',
-			lg: '976px',
-			xl: '1440px',
-		  },
-		  
-		extend: {
-			colors: {
-			pastel: {
-				greenPrimary: "#7cfc00",
-				greenHover: "rgba(124, 252, 0, .1)",
-				greyPerl: "#E6E6E6",
-				lightDark: "#434343"
-			}
-		  },
-		  width: {
-			xs: '390px',
-		  }
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+
+    extend: {
+      colors: {
+        pastel: {
+          greenPrimary: "#7cfc00",
+          greenHover: "rgba(124, 252, 0, .1)",
+          greyPerl: "#E6E6E6",
+          lightDark: "#434343",
+        },
+        blueDiscord: "#4f5d7f",
+      },
+      width: {
+        xs: "390px",
+      },
+    },
+  },
+  plugins: [],
+};
