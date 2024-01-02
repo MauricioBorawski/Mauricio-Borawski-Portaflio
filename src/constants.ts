47c91c1b47163ebb927ff217af8a086fa28bae40
Mauricio Borawski <mauricio.borawski@gmail.com>
2024-01-02 15:50:02 -0300

    WIP Experience section

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,9 @@
 export type Position = {
-  active: boolean;
   title: string;
-  time: string;
-  technologies: string;
-  summary: string;
-  achivements: string[];
+  company: string;
+  date: string;
+  description: string;
+  href: string;
 };
 
 export type Projects = {
@@ -17,34 +16,34 @@ export type Projects = {
 
 export const companies = ["cedeira", "blockfi"] as const;
 
-export type Companies = typeof companies[number];
+export type Companies = (typeof companies)[number];
 
-export type Experiences = Record<Companies, Position>;
+export type Experiences = Array<Position>;
 
-export const experiences: Experiences = {
-  cedeira: {
-    active: true,
-    title: "React Developer",
-    time: "March 2023 - Present",
-    technologies: "React, Typescript, Jest, Redux, MUI",
-    summary:
+export const experiences: Experiences = [
+  {
+    title: "Front End Developer",
+    company: "Agrology S.R.L",
+    date: "August 2023 - Present",
+    description: "",
+    href: "https://www.linkedin.com/company/agrology-agro/",
+  },
+  {
+    title: "Front-End Developer",
+    company: "Cedeira Software Factory",
+    date: "March 2023 - August 2023",
+    description:
       "Develop high-quality code with a focus on creating and maintaining comprehensive tests. I work closely with the development team to ensure the delivery of reliable and robust software. Implementing best practices and coding standards ensures our code is scalable, modular, and easily maintainable.",
-    achivements: [],
+    href: "https://www.linkedin.com/company/cedeirasf/",
   },
-  blockfi: {
-    active: false,
+  {
     title: "Front-End Engineer",
-    time: "March 2020 - December 2022",
-    technologies: "React, Typescript, Jest, Redux, Styled-Components",
-    achivements: [
-      "Develop new features used by millions of users around the world like introducing ACH and BIA Trading.",
-      "Increase test coverage from 30% to 98%.",
-      "Write documentation for internal libraries like Goblin.",
-      " Introduce new members of the team to the code base. ",
-    ],
-    summary: "",
+    company: "BlockFi",
+    date: "March 2020 - December 2022",
+    description: "",
+    href: "https://www.linkedin.com/company/blockfi/",
   },
-};
+];
 
 export const projects: Projects[] = [
   {
